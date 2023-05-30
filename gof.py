35e9e85bdd4ecba13938fa71f484400cfc74727b
Arvind <nairarvind2011@gmail.com>
2023-05-30 23:40:22 +0530

    misc changes

--- gof.py
+++ gof.py
@@ -1,4 +1,29 @@
 
 
+# create a board
 def create_board(rows, cols):
     return [[0 for _ in range (cols)] for _ in range (rows)]
+
+# TODO
+# initialise it with alive cells at random positions
+# display board
+# keep track of neighbors each cell has, pass coordinates of cell and the board
+# update board
+# def update_board(board):
+#     rows = len(board)
+#     cols = len(board[0])
+#     updated_board = create_board(rows, cols)
+#     for i in range(rows):
+#         for j in range(cols):
+#             live_neighbors = count_live_neighbors(board, i, j)
+#             if board[i][j] == 1:
+#                 if live_neighbors < 2 or live_neighbors > 3:
+#                     updated_board[i][j] = 0
+#                 else:
+#                     updated_board[i][j] = 1
+#             else:
+#                 if live_neighbors == 3:
+#                     updated_board[i][j] = 1
+#                 else:
+#                     updated_board[i][j] = 0
+#     return updated_board
