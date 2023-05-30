985e6a525f257b513f4085c328c3b9f2b268148b
Arvind <nairarvind2011@gmail.com>
2023-05-30 22:38:43 +0530

    RED: create_board() values not initialized to 0

--- test_gof.py
+++ test_gof.py
@@ -1,5 +1,6 @@
-import gof 
+import gof
 import pytest
+
 
 # Tests for create_board
 
@@ -7,5 +8,12 @@ def test_create_board_size():
     board = gof.create_board(3, 4)
     assert len(board) == 3
     assert len(board[0]) == 4
-    
-    
+
+
+def test_create_board_initialize_to_zero():
+    board = gof.create_board(2, 4)
+    for i in range(2):
+        for j in range(2):
+            assert board[i][j] == 0
+
+##
