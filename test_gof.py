cf3c861a1fe296be5cf15ac278bc0f42225a8948
Arvind <nairarvind2011@gmail.com>
2023-05-31 14:22:05 +0530

    REFACTOR: updates tests

--- test_gof.py
+++ test_gof.py
@@ -9,20 +9,15 @@ def test_create_board_size():
     assert len(board) == 3
     assert len(board[0]) == 4
 
-def test_create_board_initialize_to_zero():
-    board = gof.create_board(2, 4)
-    for i in range(2):
-        for j in range(2):
-            assert board[i][j] == 0
-
 def test_create_board_type_error():
     with pytest.raises(TypeError):
         gof.create_board('a', 'b')
 
-# def test_create_board_value_error():
-#     with pytest.raises(ValueError):
-#         gof.create_board(-3, -5)
-
+def test_create_board_value_error():
+    board = gof.create_board((-3), -4)
+    assert len(board) == 0
+    assert len(board[0]) == 0
+    
 ##
 
 # Tests for display_board
