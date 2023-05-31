134e6adff987a29042e95b8231053c9093097d2e
Arvind <nairarvind2011@gmail.com>
2023-05-31 15:21:41 +0530

    GREEN: passes all tests

--- gof.py
+++ gof.py
@@ -29,28 +29,26 @@ def count_live_neighbors(grid, row, col):
     return live_neighbor_count
 
 
-# TODO
-# initialise it with alive cells at random positions
-# keep track of neighbors each cell has, pass coordinates of cell and the board
 # update board
-# def update_board(board):
-#     rows = len(board)
-#     cols = len(board[0])
-#     updated_board = create_board(rows, cols)
-#     for i in range(rows):
-#         for j in range(cols):
-#             live_neighbors = count_live_neighbors(board, i, j)
-#             if board[i][j] == 1:
-#                 if live_neighbors < 2 or live_neighbors > 3:
-#                     updated_board[i][j] = 0
-#                 else:
-#                     updated_board[i][j] = 1
-#             else:
-#                 if live_neighbors == 3:
-#                     updated_board[i][j] = 1
-#                 else:
-#                     updated_board[i][j] = 0
-#     return updated_board
+def update_board(board):
+    rows = len(board)
+    cols = len(board[0])
+    updated_board = create_board(rows, cols)
+    for i in range(rows):
+        for j in range(cols):
+            live_neighbors = count_live_neighbors(board, i, j)
+            if board[i][j] == 1:
+                if live_neighbors < 2 or live_neighbors > 3:
+                    updated_board[i][j] = 0
+                else:
+                    updated_board[i][j] = 1
+            else:
+                if live_neighbors == 3:
+                    updated_board[i][j] = 1
+                else:
+                    updated_board[i][j] = 0
+    return updated_board
+
 
 if __name__ == "__main__":
     # initialise board
@@ -58,3 +56,6 @@ if __name__ == "__main__":
     cols = 8
     board = create_board(rows, cols)
     print(display_board(board))
+    
+    # while True:
+        
--- test_gof.py
+++ test_gof.py
@@ -9,24 +9,34 @@ def test_create_board_size():
     assert len(board) == 3
     assert len(board[0]) == 4
 
+
 def test_create_board_type_error():
     with pytest.raises(TypeError):
-        gof.create_board('a', 'b')
+        gof.create_board("a", "b")
+
 
 def test_create_board_value_error():
     board = gof.create_board((-3), -4)
     assert len(board) == 0
-    assert len(board[0]) == 0
-    
+
 ##
 
 # Tests for display_board
 
 def test_display_board_empty_board():
-    board = gof.create_board(2, 4)
-    assert gof.display_board(board) == """0 0 0 0 
+    board = [[0, 0, 0, 0], [0, 0, 0, 0]]
+    assert (
+        gof.display_board(board)
+        == """0 0 0 0 
 0 0 0 0 
 """
+    )
+
 
 ##
 
+# Tests for count_live_neighbors()
+
+def test_count_live_neighbors_two_alive():
+    board = [[0, 1, 0], [1, 0, 0], [1, 0, 0]]
+    assert gof.count_live_neighbors(board, 1, 0) == 2
